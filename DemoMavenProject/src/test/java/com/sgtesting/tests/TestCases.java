dccbf5f8d12d8924fdf62eb7fbf3a7a235922589
kavyavk73 <kavyavk73@gmail.com>
2022-09-22 10:56:17 +0530

    Initial Draft

---
+++ DemoMavenProject/src/test/java/com/sgtesting/tests/TestCases.java
@@ -0,0 +1,17 @@
+package com.sgtesting.tests;
+
+import org.testng.annotations.Test;
+
+public class TestCases {
+	@Test
+	public void method1()
+	{
+		System.out.println("it is a method");
+	}
+
+	@Test
+	public void method2()
+	{
+		System.out.println("it is a method2");
+	}
+}
